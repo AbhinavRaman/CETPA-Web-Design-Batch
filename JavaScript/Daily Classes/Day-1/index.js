3aad4e324ded4b4295ab4cb736b625db1fdc3ddb
Abhinav Raman <117099664+AbhinavRaman@users.noreply.github.com>
2025-03-17 13:37:20 +0530

    Add files via upload

---
+++ JavaScript/Daily Classes/Day-1/index.js
@@ -0,0 +1,17 @@
+console.log("hello world");
+
+var a = 20;
+a = 25
+let a = 95
+console.log(a)
+
+let b = 21;
+b = 91
+// let b = 32
+console.log(b)
+
+//Var can be ressigned and redeclared but let can be reassigned but not redeclared.
+
+const c = 30
+// c = 100
+console.log(c)
