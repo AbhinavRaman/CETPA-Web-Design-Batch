84c0216b66ce07540b024263b5c780d30c5ee99b
Abhinav Raman <117099664+AbhinavRaman@users.noreply.github.com>
2025-03-30 19:52:59 +0530

    Add files via upload

---
+++ JavaScript/Assignments/Assignment-2/script.js
@@ -0,0 +1,99 @@
+//Q1. Find Grades
+
+// function findGrades(marks){
+//     let grade;
+
+//     switch(true){
+//         case marks > 40 && marks <= 50:
+//             grade = 'A'
+//             break
+//         case marks > 30 && marks <= 40:
+//             grade = 'B'
+//             break
+//         case marks > 20 && marks <= 30:
+//             grade = 'C'
+//             break
+//         case marks > 10 && marks <= 20:
+//             grade = 'D'
+//             break
+//         case marks <= 10:
+//             grade = 'E'
+//             break
+//         default:
+//             grade = 'F'
+//     }
+//     return grade
+// }
+
+// let marks = 35
+// console.log(findGrades(marks))
+
+
+//Q.2 Get Value
+
+// let inputChar = 'z';
+
+// if (inputChar === 'p') {
+//     console.log("PrepBytes");
+// } else if (inputChar === 'z') {
+//     console.log("Zenith");
+// } else if (inputChar === 'e') {
+//     console.log("Expert Coder");
+// } else if (inputChar === 'd') {
+//     console.log("Data Structure");
+// } else {
+//     console.log("Character not found in table");
+// }
+
+//Q.3 Find the maximum out of three numbers
+
+// let a =  prompt("enter the number");
+// let b = prompt("enter the number");
+// let c = prompt("enter the number") ;
+//  if(a >= b && a >= c){
+//     console.log(a);
+
+//  }
+//  else if(b >= a && b >= c){
+//     console.log(b);
+
+//  }
+//  else{
+//     console.log(c);
+
+//  }
+
+//Q.4 Second Smallest
+
+// let a =  prompt("enter the number");
+// let b = prompt("enter the number");
+// let c = prompt("enter the number") ;
+
+// if(a > b && a < c){
+//         console.log(a);
+
+//      }
+//      else if(b > a && b < c){
+//         console.log(b);
+
+//      }
+//      else{
+//         console.log(c);
+
+//      }
+
+
+//Q.5 Check whether the triagle is acute of obtuse
+
+// var a = 20;
+// var b = 200;
+// var c = 20;
+
+// var res = a + b + c;
+// if (res >= 90) {
+//   console.log("this is an obtuseangle");
+// } 
+// else
+// {
+//   console.log(" right angle ");
+// }
